349bfa095a641ef5c05fe29a26f1ab5054e845eb
huiXinYiJi <suzheng91@163.com>
2019-12-10 16:51:10 +0800

    login page done

---
+++ assets/Script/Controller/GameController.js
@@ -0,0 +1,18 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        grid: {
+            default: null,
+            type: cc.Node
+        }
+    },
+
+    // onLoad () {},
+
+    start () {
+
+    },
+
+    // update (dt) {},
+});
--- assets/Script/Controller/LoginController.js
+++ assets/Script/Controller/LoginController.js
@@ -2,28 +2,55 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
+        loadingBar: {
+            type: cc.ProgressBar,
+            default: null
+        },
+        loginButton: {
+            type: cc.Button,
+            default: null
+        },
+        worldSceneBGM: {
+            type: cc.AudioClip,
+            default: null
+        }
     },
 
-    // onLoad () {},
+    onLoad () {
+        // 播放音频
+        this.gameSceneBGMAudioId = cc.audioEngine.play(this.worldSceneBGM, true, 1)
+        this.loadtext = (this.loadingBar.node.getChildByName('loadtext')).getComponent(cc.Label)
+    },
+
+    onLogin () {
+        this.loginButton.node.active = false // 隐藏登录按钮
+        this.loadingBar.node.active = true // 显示进度条
+        this.loadingBar.progress = 0 // 进度条初始值为0
+        this.loadtext.string = ''
+        cc.director.preloadScene('Game', (completedCount, totalCount, item) => {
+            // console.log(completedCount, totalCount)
+            this.loadingBar.progress = completedCount / totalCount
+            this.loadtext.string = `${completedCount / totalCount * 100}%`
+            // 3秒后跳转到游戏界面
+            setTimeout(() => {
+                cc.director.loadScene("Game")
+                this.loadingBar.node.active = false
+                this.loginButton.node.active = true
+                this.loadtext.string = ''
+            }, 2000)
+        }, (err, asset) => {
+            console.log('preload Game done')
+        })
+    },
 
     start () {
 
     },
 
     // update (dt) {},
+
+    onDestroy() {
+        // 停止播放音频
+        cc.audioEngine.stop(this.gameSceneBGMAudioId)
+    }
 });
